c600fbb5b6c23d43dad172189ec557de1d422fed
kittyMons <puteriaqilah2609@gmail.com>
2024-07-06 09:37:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,4 @@
-import streamlit as st
-import requests
-from openai import OpenAI
 
-
-client = OpenAI(api_key = st.secrets['OPENAI_API_KEY'])
-
-def ai_suggestion(thumbnail,price):
-  response = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-              {
-                "role":"user",
-                "content":[{
-                "type":"text", "text":"What is in this image?",
-                "text":"is this product worth RM" + str(price) + "xxx price?"
-                },
-                {
-                    "type":"image_url",
-                    "image_url":{
-                        "url":thumbnail
-                    }
-                }
-            ]
-        }
-    ],
-    max_tokens=50,
-  )
-
-  return response.choices[0].message.content
-
-st.title('Cart')
-
-uploaded_files = st.file_uploader("Upload pictures of clothes or accessories", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
-
-response = requests.get('https://dummyjson.com/carts')
-product_name=response.json()['carts'][0]['products'][0]['title']
-product_price = response.json()['carts'][0]['products'][1]['price']
-product_thumbnail = response.json()['carts'][0]['products'][1]['thumbnail']
 import streamlit as st
 import requests
 from openai import OpenAI
@@ -46,7 +8,7 @@ client = OpenAI(api_key = st.secrets['OPENAI_API_KEY'])
 st.title("What's your outfit today?")
 
 def ai_suggestion(images, occasion):
-  response = openai.ChatCompletion.create(
+  response = OpenAI.ChatCompletion.create(
   model="gpt-4",
   messages = [
       {
@@ -86,3 +48,4 @@ if st.button("Get Suggestion"):
 
         st.write(suggestion)
     else:
+      st.write(Please upload image)
